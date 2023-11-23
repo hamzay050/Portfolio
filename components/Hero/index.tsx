21da7ab72334d134cf9ef9a3200c2c9fb36db93a
Hamza Yousaf <hamzay050@gmail.com>
2023-11-24 03:23:11 +0500

    hero section improved bugfixed on build  typing

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import ParallaxParticles from "./ParallaxParticles";
 
+import { Fade, Zoom } from "react-awesome-reveal";
+
 const Hero = () => {
-  const isClient = typeof window !== "undefined";
+  const text =
+    "Welcome to the world of cutting-edge technology, where innovation meets expertise. I'm Hamza Yousaf, a seasoned Full-Stack Software Engineer with 3 years of hands-on experience in crafting exceptional mobile and web solutions. Let's embark on a journey to create digital magic together!";
+
   return (
     <>
       <section
@@ -21,30 +27,38 @@ const Hero = () => {
                 className="wow fadeInUp mx-auto max-w-[800px] text-center"
                 data-wow-delay=".2s"
               >
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Crafting Digital Excellence as a Full-Stack Software Engineer
-                </h1>
-                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                  Welcome to the world of cutting-edge technology, where
-                  innovation meets expertise. I'm Hamza Yousaf, a seasoned
-                  Full-Stack Software Engineer with 3 years of hands-on
-                  experience in crafting exceptional mobile and web solutions.
-                  Let's embark on a journey to create digital magic together!
-                </p>
-                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"
-                    className="rounded-md bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
-                  >
-                    Hire Me 💼
-                  </Link>
-                  <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"
-                    className="rounded-md bg-black/20 px-8 py-4 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
-                  >
-                    Explore My Work 📂
-                  </Link>
-                </div>
+                <Fade duration={5000}>
+                  <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                    Crafting Digital Excellence as a Full-Stack Software
+                    Engineer
+                  </h1>
+                </Fade>
+                <Zoom duration={3000}>
+                  <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+                    Welcome to the world of cutting-edge technology, where
+                    innovation meets expertise. I'm Hamza Yousaf, a seasoned
+                    Full-Stack Software Engineer with 3 years of hands-on
+                    experience in crafting exceptional mobile and web solutions.
+                    Let's embark on a journey to create digital magic together!
+                  </p>
+                </Zoom>
+
+                <Fade duration={3000}>
+                  <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                    <Link
+                      href="https://nextjstemplates.com/templates/saas-starter-startup"
+                      className="rounded-md bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                    >
+                      Hire Me 💼
+                    </Link>
+                    <Link
+                      href="https://github.com/NextJSTemplates/startup-nextjs"
+                      className="rounded-md bg-black/20 px-8 py-4 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
+                    >
+                      Explore My Work 📂
+                    </Link>
+                  </div>
+                </Fade>
               </div>
             </div>
           </div>
